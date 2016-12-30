fa35b2887af6bdc199f796a44a8ce596798e9539
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2016-12-30 12:29:42 -0700

    Use ARG (instead of ENV) where appropriate

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ set -o errexit
 SCRIPT_DIR="$(dirname $(readlink -f ${0}))"
 
 IMAGE_NAME="phlak/openvpn"
-TAG="$(grep 'ENV OVPN_VERSION' Dockerfile | awk '{print $3}')"
+TAG="$(grep 'ARG OVPN_VERSION' Dockerfile | awk -F = '{print $2}')"
 
 ## SCRIPT USAGE
 ########################################
